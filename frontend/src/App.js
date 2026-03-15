e5d70df5aa94bdef477ea12e70f821ab3f66309f
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2026-03-15 15:31:12 +0600

    wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -67,7 +67,7 @@ export default class App extends React.Component {
         <div className="container-fluid">
           <div className="row">
             <div className="col-xs-12 col-sm-8 col-md-8 offset-md-2">
-              <h1>My Todo List</h1>
+              <h1>My Todo List Item</h1>
               <div className="todo-app">
                 <AddTodo handleAddTodo={this.handleAddTodo} />
                 <TodoList
